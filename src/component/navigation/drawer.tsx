8056a4f30cd4b2372d0a726bd0b8bb4f93a54e05
Dangoz <71613713+Dangoz@users.noreply.github.com>
2021-08-18 01:34:40 -0700

    drawer css

--- src/component/navigation/drawer.tsx
+++ src/component/navigation/drawer.tsx
@@ -20,34 +20,34 @@ const DrawerMenu = ({ visible, setVisible }: {
   return (
     <>
       <Drawer className={DrawerStyle.drawer} visible={visible}
-        onClose={onClose} closable={false} width={220} placement={'left'}
+        onClose={onClose} closable={false} width={230} placement={'left'}
         bodyStyle={{ padding: 0 }}
       >
         <div className={DrawerStyle.wrapper}>
           <Menu className={DrawerStyle.menuWrapper} defaultOpenKeys={['sub1']}
-            style={{ width: 220 }} mode={'inline'} 
+            style={{ width: 230 }} mode={'inline'}
             theme={'dark'} onSelect={itemSelect}>
 
             <Menu.Item key='1' icon={<ScheduleOutlined />} className={DrawerStyle.item}>
-              Schedule
+              <span className={DrawerStyle.text}>SCHEDULE</span>
             </Menu.Item>
 
             <Menu.Divider />
 
             <SubMenu key='sub1' title='Generation 1' className={DrawerStyle.subMenu} >
-              <Menu.Item key='2' className={DrawerStyle.item}>pipi</Menu.Item>
-              <Menu.Item key='3' className={DrawerStyle.item}>nasa</Menu.Item>
-              <Menu.Item key='4' className={DrawerStyle.item}>pipi</Menu.Item>
-              <Menu.Item key='5' className={DrawerStyle.item}>nasa</Menu.Item>
-              <Menu.Item key='6' className={DrawerStyle.item}>pipi</Menu.Item>
-              <Menu.Item key='7' className={DrawerStyle.item}>nasa</Menu.Item>
-              <Menu.Item key='8' className={DrawerStyle.item}>pipi</Menu.Item>
+              <Menu.Item key='2' className={DrawerStyle.item}><span className={DrawerStyle.text}>Pipkin Pippa</span></Menu.Item>
+              <Menu.Item key='3' className={DrawerStyle.item}><span className={DrawerStyle.text}>nasa</span></Menu.Item>
+              <Menu.Item key='4' className={DrawerStyle.item}><span className={DrawerStyle.text}>pipi</span></Menu.Item>
+              <Menu.Item key='5' className={DrawerStyle.item}><span className={DrawerStyle.text}>nasa</span></Menu.Item>
+              <Menu.Item key='6' className={DrawerStyle.item}><span className={DrawerStyle.text}>pipi</span></Menu.Item>
+              <Menu.Item key='7' className={DrawerStyle.item}><span className={DrawerStyle.text}>nasa</span></Menu.Item>
+              <Menu.Item key='8' className={DrawerStyle.item}><span className={DrawerStyle.text}>pipi</span></Menu.Item>
             </SubMenu>
 
             <Menu.Divider />
 
             <Menu.Item key='99' icon={<HomeOutlined />} className={DrawerStyle.item}>
-              Home Site
+              <span className={DrawerStyle.text}>HOME SITE</span>
             </Menu.Item>
           </Menu>
         </div>
