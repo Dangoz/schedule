aa61ca3572de377e5b6daa47cd799d28aec20d1b
Dangoz <71613713+Dangoz@users.noreply.github.com>
2021-08-19 01:48:11 -0700

    clean up

--- src/component/navigation/drawer.tsx
+++ src/component/navigation/drawer.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import { useState } from 'react'
-import { Drawer, Menu, Button } from 'antd'
+import { Drawer, Menu } from 'antd'
 import DrawerStyle from '@/styles/navigation/drawer.module.css'
 import { ScheduleOutlined, HomeOutlined } from '@ant-design/icons'
 import IProfile from '@/interfaces/profile.interface'
@@ -20,7 +20,7 @@ const DrawerMenu = ({ visible, setVisible, profiles }: {
   }
 
   const itemSelect = async () => {
-    // setVisible(false);
+    setVisible(false);
   }
 
   return (
@@ -41,18 +41,10 @@ const DrawerMenu = ({ visible, setVisible, profiles }: {
             <Menu.Divider />
 
             <SubMenu key='sub1' title='Generation 1' className={DrawerStyle.subMenu} >
-              {/* <Menu.Item key='dwadaw' className={DrawerStyle.item}><div className={DrawerStyle.text}>ashelia</div></Menu.Item>
-              <Menu.Item key='3' className={DrawerStyle.item}><div className={DrawerStyle.text}>nasa</div></Menu.Item>
-              <Menu.Item key='4' className={DrawerStyle.item}><div className={DrawerStyle.text}>michiru</div></Menu.Item>
-              <Menu.Item key='5' className={DrawerStyle.item}><div className={DrawerStyle.text}>tenma</div></Menu.Item>
-              <Menu.Item key='6' className={DrawerStyle.item}><div className={DrawerStyle.text}>pipi</div></Menu.Item>
-              <Menu.Item key='7' className={DrawerStyle.item}><div className={DrawerStyle.text}>uruka</div></Menu.Item>
-              <Menu.Item key='8' className={DrawerStyle.item}><Link href='/v/pipi'><div className={DrawerStyle.text}>iori</div></Link></Menu.Item> */}
               {
                 g1talents.map(talent => (
                   <Menu.Item key={`${talent.name}`} className={DrawerStyle.item}
-                    icon={<div className={DrawerStyle.photo}><img className={DrawerStyle.photoImg} src={`${talent.photo}`}/></div>}>
-
+                    icon={<div className={DrawerStyle.photo}><img className={DrawerStyle.photoImg} src={`${talent.photo}`} /></div>}>
                     <Link href={`/v/${talent.href}`}><div className={DrawerStyle.text}>{`${talent.name}`}</div></Link>
                   </Menu.Item>
                 ))
