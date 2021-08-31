f501cf3a74286ec903fad6763c9fdfeddda35998
Dangoz <71613713+Dangoz@users.noreply.github.com>
2021-08-31 05:21:26 -0700

    rotation adjustment, z-axis height

--- src/component/profile/option-menu.tsx
+++ src/component/profile/option-menu.tsx
@@ -23,19 +23,19 @@ const OptionMenu = ({ talent }: { talent: IProfile }) => {
     console.log('x', e.clientX, 'y', e.clientY);
 
     pbutton.current.style.transform = `translate3d(${getOffset(e.clientX, window.innerWidth, 28)}px, 
-    ${getOffset(e.clientY, window.innerHeight, 10)}px, 0px)
+    ${getOffset(e.clientY, window.innerHeight, 10)}px, -50px)
     rotateX(${getOffset(e.clientY, window.innerHeight, 3)}deg) 
     rotateY(${getOffset(e.clientX, window.innerWidth, 9)}deg)`;
     vbutton.current.style.transform = `translate3d(${getOffset(e.clientX, window.innerWidth, 33)}px, 
-    ${getOffset(e.clientY, window.innerHeight, 13)}px, 0px) 
+    ${getOffset(e.clientY, window.innerHeight, 13)}px, -50px) 
     rotateX(${getOffset(e.clientY, window.innerHeight, 3)}deg) 
     rotateY(${getOffset(e.clientX, window.innerWidth, 9)}deg)`;
     photoRef.current.style.transform = `translate3d(${getOffset(e.clientX, window.innerWidth, 57)}px, 
-    ${getOffset(e.clientY, window.innerHeight, 20)}px, 0px) 
+    ${getOffset(e.clientY, window.innerHeight, 20)}px, -100px) 
     rotateX(${getOffset(e.clientY, window.innerHeight, 3)}deg) 
-    rotateY(${getOffset(e.clientX, window.innerWidth, 9)}deg)`;
+    rotateY(${getOffset(e.clientX, window.innerWidth, 25)}deg)`;
     nameRef.current.style.transform = `translate3d(${getOffset(e.clientX, window.innerWidth, 100)}px, 
-    ${getOffset(e.clientY, window.innerHeight, 30)}px, 50px) 
+    ${getOffset(e.clientY, window.innerHeight, 30)}px, -50px) 
     rotateX(${getOffset(e.clientY, window.innerHeight, 3)}deg) 
     rotateY(${getOffset(e.clientX, window.innerWidth, 9)}deg)`;
   }
