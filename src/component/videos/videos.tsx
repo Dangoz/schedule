046cd0608d865385bc6c6bf9a460585d64cc0e5f
Dangoz <71613713+Dangoz@users.noreply.github.com>
2021-09-03 14:55:23 -0700

    start on videos

--- src/component/videos/videos.tsx
+++ src/component/videos/videos.tsx
@@ -1,13 +1,17 @@
 import ICompleteVideo from '@/interfaces/complete-video.interface'
 import { useState, useEffect } from 'react'
+import { Tag, Pagination } from 'antd'
+import VideoStyle from '@/styles/videos/video.module.css'
 
 const Videos = ({ videos }: { videos: ICompleteVideo[] }) => {
-  return (
+  const [data, setData] = useState(videos)
+
+  return (<>
     <div>
-      {videos.length} <br/>
-      {JSON.stringify(videos, null, 2)}
+      {videos.length} <br />
+      {false && JSON.stringify(videos, null, 2)}
     </div>
-  )
+  </>)
 }
 
 export default Videos
