30ed005326d326f671c056b9744d6ca4cd1de9fd
Dangoz <71613713+Dangoz@users.noreply.github.com>
2022-04-10 14:50:29 -0700

    temporary redirect, considering revamp

---
+++ src/pages/_middleware.ts
@@ -0,0 +1,8 @@
+// pages/_middleware.ts
+import type { NextFetchEvent, NextRequest } from 'next/server'
+import { NextResponse } from 'next/server'
+
+export function middleware(request: NextRequest, event: NextFetchEvent) {
+
+  return NextResponse.redirect("https://phase-connect.com/")
+}
